707665958a2b966597996a268b5ef7e2dd8311c3
gnome <200718288+ncuwiki@users.noreply.github.com>
2025-03-04 23:54:52 -0500

    set up tha content submodule

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "ncuwiki.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
